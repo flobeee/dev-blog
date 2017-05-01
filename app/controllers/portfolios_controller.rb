6b631f9f4568bc23ec2f60e72b00eadcdcd1b0f0
Matthew Wint <mwwint@gmail.com>
2017-05-01 03:53:01 -0700

    implemented destroy

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,18 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+
+    # perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # destroy/delete the lookup
+    @portfolio_item.destroy
+
+    # redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
+    end
+  end
 end
